2dfb957ae6a34933df63c186aff488b2b2adbe5c
theupselling <theupselling@gmail.com>
2023-08-29 01:48:19 +0500

    added delete option

--- main.py
+++ main.py
@@ -100,12 +100,13 @@ for article in articles:
 
 
         file_name = generate_news_image(article.urlToImage, news_with_asterisks)
-        file_name_story=generate_news_image_potraite(article.urlToImage, news_with_asterisks,None)
+        # file_name_story=generate_news_image_potraite(article.urlToImage, news_with_asterisks,None)
        
         # print("file_name:", file_name)
         photo_id=post_to_facebook(file_name, article.description, article.url)
         # post_insagram(cl,file_name,file_name_story ,article.description, article.url)
-        # os.remove(file_name)
+        os.remove(file_name)
+
 
         
     
